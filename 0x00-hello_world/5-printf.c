e0dc421aa37d8d29651709fc41aec77190ff8385
Mohamed Coulibaly <ml.coulibaly99@gmail.com>
2023-08-10 09:40:04 -0700

    task5ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A programm that print using printf function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
