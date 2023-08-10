ba1b24a73fa4f677f5b0c039d4da9f6a6768be1e
Mohamed Coulibaly <ml.coulibaly99@gmail.com>
2023-08-10 09:35:22 -0700

    task4ok

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
