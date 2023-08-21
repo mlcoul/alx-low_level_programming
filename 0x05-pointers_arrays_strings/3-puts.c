bbca779c8558e541f25fe555f4cb57f41943b45a
Mohamed Coulibaly <ml.coulibaly99@gmail.com>
2023-08-21 05:12:57 -0700

    task 3 outputok

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <unistd.h>
 
-
 /**
- * _puts - function that print a string
- *@str: string to be return
- *Return: a string
+ * _puts - Entry point
+ * @str: pointer the sstring we want to print
+ *Return: no return
  */
-
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0 ; i++)
+	i = 0;
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
