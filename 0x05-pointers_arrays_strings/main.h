95e20004b201854928b426c17eccb5dee9851eee
Mohamed Coulibaly <ml.coulibaly99@gmail.com>
2023-08-21 19:20:28 -0700

    putchar problem solved

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,7 +14,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-
 
 #endif
